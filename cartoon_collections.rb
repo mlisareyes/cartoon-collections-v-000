38350f8b34fa3d3523d6582d401733a9c306f19a
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-06-24 03:23:25 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,11 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each.with_index(1) do |dwarf, index|
+    puts "#{index}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+planeteer_calls.map {|name| name.capitalize + "!"}
 end
 
 def long_planeteer_calls# code an argument here
