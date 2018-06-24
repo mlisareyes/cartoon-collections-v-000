1d6336a32523f4991838f98f886802bbed977981
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-06-24 03:39:47 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ def long_planeteer_calls(calls_short)
 end
 
 def find_the_cheese(cheddar_cheese)
-  # the array below is here to help
+  
   cheese_types = ["cheddar", "gouda", "camembert"]
   end
 end
